c3e4be81932212728dec2c2119e054c2c51fdb73
trickyj <pop.tricky@gmail.com>
2023-11-08 16:21:47 +0530

    updated my compare dot py file

---
+++ compare.py
@@ -0,0 +1,9 @@
+x = int(input("what's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is grater than y")
+if x == y:
+    print("x is equals to y")
